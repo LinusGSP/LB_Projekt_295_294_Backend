618e916cbdd9d1377c4c5adb6f8839d04c76c12d
Linus <schoenbaechler.linus@gmail.com>
2022-09-18 12:33:44 +0200

    Project Structure

--- src/main/java/ch/project/quizme/exceptions/LanguageFailedSaveException.java
+++ src/main/java/ch/project/quizme/exceptions/LanguageFailedSaveException.java
@@ -1,7 +1,4 @@
 package ch.project.quizme.exceptions;
-
-import ch.project.quizme.controller.LanguageController;
-
 public class LanguageFailedSaveException extends RuntimeException{
     public LanguageFailedSaveException(String name){
         super("The Language '" + name + "' could not be saved.");
--- src/main/java/ch/project/quizme/exceptions/LanguageNotFoundException.java
+++ src/main/java/ch/project/quizme/exceptions/LanguageNotFoundException.java
@@ -1,7 +1,5 @@
 package ch.project.quizme.exceptions;
 
-import ch.project.quizme.databases.Language;
-
 public class LanguageNotFoundException extends RuntimeException{
     public LanguageNotFoundException(Integer id){
         super("The Language with id: " + id + " could not be found");
