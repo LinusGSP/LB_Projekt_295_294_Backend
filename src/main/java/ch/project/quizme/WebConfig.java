a49942c5e046fdb4de5328378fef7d16627d2d0d
Linus <schoenbaechler.linus@gmail.com>
2022-09-27 11:36:35 +0200

    Updated web config for get post delete methods

--- src/main/java/ch/project/quizme/WebConfig.java
+++ src/main/java/ch/project/quizme/WebConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "DELETE");
     }
 }
