c44cf3dfae6b651f8520f6f63d8eb3f8161fbb5f
Linus <schoenbaechler.linus@gmail.com>
2022-10-05 08:39:42 +0200

    Added Applicazion

---
+++ src/main/java/ch/project/quizme/QuizmeApplication.java
@@ -0,0 +1,13 @@
+package ch.project.quizme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizmeApplication.class, args);
+    }
+
+}
